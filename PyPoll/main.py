9177a5defbb6cb6d1299396bb32bacc103cce7bd
Francisca Escamilla <francis.escamilla@gmail.com>
2021-05-08 02:23:47 -0500

    PyPoll script now has list of candidates and votes per each, pending to calculate percentages and print results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,8 +5,9 @@ import csv
 #set variables here
 total_votes= 0
 candidate_list=[]
-cpercent=0.0
 cvotes_list=[]
+cpercent=0.0
+
 
 #Specify the file to read from
 csvpath = os.path.join('Resources', 'election_data.csv')
@@ -24,8 +25,16 @@ with open(csvpath) as csvfile:
     # Read each row of data after the header
     for row in csvreader:
         total_votes += 1 #increase total counts
+        candidate = row[2]
         #if candidate name is in the list add to its vote count, else add their name and start count
-        
+        if candidate in candidate_list:      
+            cindex =candidate_list.index(candidate) #find the index for the candidate name and use it to increase vote count
+            cvotes_list[cindex]+=1
+        else:
+            candidate_list.append(candidate)
+            cvotes_list.append(1) #give the newly added candidate 1 vote
+
+print(candidate_list, cvotes_list)
 print("Election Results")
 print("---------------------------")
 print(f"Total Votes: {total_votes}")           
